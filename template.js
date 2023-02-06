d35b7a44a8f621dfbceb9c1cde92b2188d1557ae
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-02-06 13:05:52 +0100

    string opgave oprettet

--- template.js
+++ template.js
@@ -4,3 +4,20 @@ const animal = "cat";
 const catName = "Mandu";
 
 console.log(`My name is ${person}.\nI have a ${animal} called ${catName}.`);
+
+const name = "Albus Percival Wulfric Brian Dumbledore";
+const len = name.length;
+const letter = name[6];
+
+console.log(`${name} is ${len} characters long.`);
+console.log(`the first letter of ${name} is ${letter}.`);
+
+const str1 = "   There is     space here \n  ";
+const str2 = str1.trim();
+
+console.log(str2);
+
+const fullName = "Alberte Jasmin Nielsen";
+const firstName = fullName.substring(0, 7);
+
+console.log(`fistname is: _${firstName}_`);
