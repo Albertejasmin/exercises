1f6a59b160ead4f4dfa874873074aaf902ac6633
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-02-08 17:14:52 +0100

    flere array comit

--- 11C.02.02_typewriter/typewriter.js
+++ 11C.02.02_typewriter/typewriter.js
@@ -5,13 +5,15 @@ document.querySelector("#typewriter").textContent = "";
 console.log("str_" + str + "_");
 console.log("third letter", str[2]);
 
-// Konstanter til lyde
-const lyd1 = document.querySelector("#typekey1");
-const lyd2 = document.querySelector("#typekey2");
-
 let iterator = 0;
 let maxNumberOfIterations;
 let playing = true;
+
+// Konstanter til lyde
+const lyd1 = document.querySelector("#typekey1");
+const lyd2 = document.querySelector("#typekey2");
+// random lyd
+let ranLyd = Math.floor(Math.random() * 2) + 1;
 
 // Tilføjer click på start knappen og kører initLoop funktionen
 document.querySelector("#start").addEventListener("click", initLoop);
@@ -32,6 +34,7 @@ function loop() {
   document.querySelector("#typewriter").textContent += str[iterator];
   iterator++;
   lyd1.play();
+  lyd2.play();
   //   fortæller hvis antallet af iterationer er mindre eller = vores maxietrationer skal den køre loop funktionen
   if (iterator <= maxNumberOfIterations && playing === true) {
     //   makes delay, tager fat i funktionen loop og forsinker med 200ms
