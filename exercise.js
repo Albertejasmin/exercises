9aa88c762621933898bc425680c3bf8953568f6c
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-02-09 11:12:42 +0100

    forloop øvelse

--- exercise.js
+++ exercise.js
@@ -2,20 +2,20 @@
 
 // OBEJCTS ØVELSER
 
-const student1 = {
-  firstName: "Harry",
-  lastName: "Potter",
-};
-const student2 = {
-  firstName: "Harry",
-  lastName: "Potter",
-};
+// const student1 = {
+//   firstName: "Harry",
+//   lastName: "Potter",
+// };
+// const student2 = {
+//   firstName: "Harry",
+//   lastName: "Potter",
+// };
 
-student1.firstName = student2.firstName;
-student1.lastName = student2.lastName;
+// student1.firstName = student2.firstName;
+// student1.lastName = student2.lastName;
 
-if (student1 === student2) {
-  console.log("They are the same!");
-} else {
-  console.log("They are not ...");
-}
+// if (student1 === student2) {
+//   console.log("They are the same!");
+// } else {
+//   console.log("They are not ...");
+// }
---
+++ forloop.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// // LOOP ØVELSE
+// // From 0-9
+// for (let counter = 0; counter < 10; counter++) {
+//   // console.log(counter);
+// }
+// // console.log(`After the loop the counter is ${counter}`);
+
+// // From 1-10
+// for (let counter1 = 1; counter1 < 10; counter1++) {
+//   console.log(counter1);
+// }
+// // From 10 to 0, and after 0 it console.logs the text: “liftoff”
+// for (let counter1 = 10; counter1 >= 0; counter1--) {
+//   console.log(counter1);
+//   if (counter1 < 1) {
+//     console.log("lidtoff");
+//   }
+// }
+
+// // From 1 to 19, but only counts odd numbers (no if-statement, change the increment!)
+// // +=2 --> så plusser den med 2 hver gang, istedte for 1.
+// for (let counter1 = 1; counter1 < 20; counter1 += 2) {
+//   console.log(counter1);
+// }
+
+// // From 1 to 16777216, but doubling each time (2, 4, 8, 16 …)
+
+// for (let counter1 = 1; counter1 <= 16777216; counter1 *= 2) {
+//   console.log(counter1);
+// }
+
+// // From 111 to 138, in steps of 3 (meaning the second number is 114 (111+3))
+// for (let counter1 = 111; counter1 <= 138; counter1 += 3) {
+//   console.log(counter1);
+
+// From 100 down to 0 in steps of 10
+for (let counter1 = 100; counter1 >= 0; counter1 -= 10) {
+  console.log(counter1);
+}
